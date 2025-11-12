6bb0113cc89c61ab5706ecfac00ce97cdb5e5d01
Cipherdam <cipherdam@gmail.com>
2025-11-12 09:21:57 -0500

    Updated the page to contain a grid instead of constantly nesting layouts. Easier in XML but I like to see what I can do in C#

--- Pages/PlantEntry.xaml.cs
+++ Pages/PlantEntry.xaml.cs
@@ -12,14 +12,44 @@ H2
 
 */
 public partial class PlantEntry : ContentPage {
-    
-    Button addPlantButton;
-    VerticalStackLayout layout;
+
+    readonly Button? plantButton = null;
+    private readonly VerticalStackLayout vsLayout;
+    // CLEAN:ONBUILD
+    // NOT using this right now but saving it for later, 
+    // HorizontalStackLayout hsLayout;
     public PlantEntry() {
 
         this.BackgroundColor = Color.FromArgb("512bdf");
 
-        layout = new VerticalStackLayout {
+        var pageGrid = new Grid {
+            Margin = new Thickness(15),
+            Padding = new Thickness(30),
+            ColumnDefinitions = {
+                new ColumnDefinition(GridLength.Star),
+                new ColumnDefinition(GridLength.Star)
+            },
+            RowDefinitions = { 
+                new RowDefinition(GridLength.Auto), //Row 0
+                new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto),
+                new RowDefinition(GridLength.Auto), //Row 6
+            }
+        };
+
+        var pageHeader = new Label {
+            Text = "Plant Information",
+            FontSize = 30,
+            TextColor = Colors.DarkGreen,
+            HorizontalOptions = LayoutOptions.Center
+        };
+
+
+
+        vsLayout = new VerticalStackLayout {
 
             Margin = new Thickness(15, 15, 15, 15),
             Padding = new Thickness(30, 30, 30, 30),
@@ -41,14 +71,47 @@ public partial class PlantEntry : ContentPage {
 
             }
         };
-        addPlantButton = new Button { Text = "Save Entry", BackgroundColor = Color.FromRgb(0, 148, 255) };
-		layout.Children.Add(addPlantButton);
-        Content = layout;
 
-        addPlantButton.Clicked += (sender, e) => {
-            Debug.WriteLine("Clicked");
-        };
+        // CLEAN:ONBUILD
+        // NOT using this right now but saving it for later, 
+        // 
+        //hsLayout = new HorizontalStackLayout {
+        //    Margin = new Thickness(15, 15, 15, 15),
+        //    Padding = new Thickness(30, 30, 30, 30),
+        //    Children = {
+        //        new Label  { Text = "Plant Information", FontSize = 30, TextColor = Color.FromRgb(255, 255, 100) },
+        //        new Label  { Text = "PlaceHolder", FontSize = 30, TextColor = Color.FromRgb(255, 255, 100) },
 
+        //    }
+        //};
         
+        try {
+            pageGrid.Add(pageHeader, 0, 0);
+            pageGrid.SetColumnSpan(pageHeader, 2);
+
+            pageGrid.Add(vsLayout, 0, 1);
+            //pageGrid.Add(vsLayout, 0, 2); //this caused an overflow but im saving the location a learning experience haha
+
+            plantButton = new Button { Text = "Save Entry", BackgroundColor = Color.FromRgb(0, 148, 255) };
+            var pressedCount = 0;
+            plantButton.Clicked += (sender, e) => {
+                pressedCount++;
+                Debug.WriteLine("Clicked");
+                Console.WriteLine("Clicked");
+            };
+            //Saving Button
+            pageGrid.Add(plantButton, 0, 2);
+            pageGrid.SetColumnSpan(plantButton, 2);
+            //TODO: Add Save functionality
+            pageGrid.RowDefinitions.Add(new RowDefinition(GridLength.Auto));
+
+            Content = new ScrollView { Content = pageGrid };
+        }
+        catch(Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
+
+
+
     }
 }
