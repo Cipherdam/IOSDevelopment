31eeba91152dbd6c370d8935180335bbee77bfa1
Cipherdam <cipherdam@gmail.com>
2025-11-13 09:41:48 -0500

    Updated the form to contain more data on entry.

--- Pages/PlantEntry.xaml.cs
+++ Pages/PlantEntry.xaml.cs
@@ -14,7 +14,7 @@ H2
 public partial class PlantEntry : ContentPage {
 
     readonly Button? plantButton = null;
-    private readonly VerticalStackLayout vsLayout;
+    private readonly VerticalStackLayout vsLayout, vsLayoutRight, vsLayoutLeft;
     // CLEAN:ONBUILD
     // NOT using this right now but saving it for later, 
     // HorizontalStackLayout hsLayout;
@@ -49,7 +49,7 @@ public partial class PlantEntry : ContentPage {
 
 
 
-        vsLayout = new VerticalStackLayout {
+        vsLayoutLeft = new VerticalStackLayout {
 
             Margin = new Thickness(15, 15, 15, 15),
             Padding = new Thickness(30, 30, 30, 30),
@@ -62,12 +62,46 @@ public partial class PlantEntry : ContentPage {
                 new Label(),// Need to figure out what page breaks or line separaters are
                 new Label(), //Same here
                 new Label(), // and here
-                new Label{Text = "Add Area Temperature", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Suggested Area Temperature", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
                 new Entry{Text = "0", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
-                new Label{Text = "Add Plant Temperature", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Suggested Plant Temperature", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
                 new Entry{Text = "0", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
-                new Label{Text = "Add Humidity", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Stage", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "Seedling", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Preferred Soil", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "Well Drained Loamy soil in rich organic matter", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Lighting Conditions", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "Partial Sun", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+
+            }
+        };
+        vsLayoutRight = new VerticalStackLayout {
+
+            Margin = new Thickness(15, 15, 15, 15),
+            Padding = new Thickness(30, 30, 30, 30),
+            Children = {
+                new Label { Text = "Plant Zone", FontSize = 30, TextColor = Color.FromRgb(255, 255, 100) },
+                new Label{Text = "Planing Season", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "Spring", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry (),
+                new Label { Text = "Height", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry (),
+                new Label { Text = "Plant Family", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label(),// Need to figure out what page breaks or line separaters are
+                new Label(), //Same here
+                new Label(), // and here
+                new Label{Text = "Flower", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
                 new Entry{Text = "0", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Origin", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "0", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Toxicity", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "Animals and People", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Lighting Conditions", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "Partial Sun", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Toxicity", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "Animals and People", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Label{Text = "Notes", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
+                new Entry{Text = "", FontSize = 30, TextColor = Color.FromRgb(255, 255, 255) },
 
             }
         };
@@ -84,13 +118,14 @@ public partial class PlantEntry : ContentPage {
 
         //    }
         //};
-        
+
         try {
             pageGrid.Add(pageHeader, 0, 0);
             pageGrid.SetColumnSpan(pageHeader, 2);
 
-            pageGrid.Add(vsLayout, 0, 1);
-            //pageGrid.Add(vsLayout, 0, 2); //this caused an overflow but im saving the location a learning experience haha
+            //Adding Layouts to Grid
+            pageGrid.Add(vsLayoutLeft, 0, 1);
+            pageGrid.Add(vsLayoutRight, 1, 1);
 
             plantButton = new Button { Text = "Save Entry", BackgroundColor = Color.FromRgb(0, 148, 255) };
             var pressedCount = 0;
